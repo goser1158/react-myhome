82a49898b5cda05c53a21200f859513850e44bc6
goser gao <goser1@163.com>
2019-03-13 15:49:46 +0800

    my react project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React  from  'react'
+
+class  App  extends React.Component{
+
+    render(){
+        
+        return (
+          <div>这是项目根目录</div>
+        )
+    }
+}
+
+export default App
